e614a30b865905ae2c84b8d67a43503a5a41c4c6
kiccer <1072907338@qq.com>
2020-10-17 16:28:25 +0800

    [update] 更新演示页

--- miniprogramRoot/pages/home/index.js
+++ miniprogramRoot/pages/home/index.js
@@ -9,6 +9,9 @@ Page({
      * 页面的初始数据
      */
     data: {
+        toggle: {
+            show: false
+        },
         canvas: null
     },
 
@@ -30,6 +33,53 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+    saveImg () {
+        canvas.saveToAlbum()
+    },
+
+    showPost () {
+        this.setData({
+            'toggle.show': true
+        })
+
         // 创建
         canvas.create({
             query: '.poster-canvas', // 必传，canvas元素的查询条件
@@ -129,44 +179,5 @@ Page({
         }).catch(err => {
             console.log('绘制失败！', err)
         })
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    },
-
-    saveImg () {
-        canvas.saveToAlbum()
     }
 })
