5ec92be19bc47cbbf19a19d3d04428fd96d063ce
kiccer <1072907338@qq.com>
2021-07-08 10:14:56 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 const path = require('path')
+const glob = require('glob')
 
 module.exports = {
-    entry: './plugin/wx-canvas-2d.js',
+    // entry: './plugin/index.js',
+    entry: [
+        './plugin/index.js',
+        ...glob.sync('./plugin/series/*.js'),
+        ...glob.sync('./plugin/utils/*.js')
+    ],
     output: {
-        path: path.resolve(__dirname, 'plugin'),
-        filename: 'wx-canvas-2d.min.js',
-        libraryTarget: 'umd',
-        library: 'WxCanvas2d'
+        path: path.resolve(__dirname, 'plugin/build'),
+        // filename: 'index.min.js'
+        filename: 'index.min.js'
+        // libraryTarget: 'umd',
+        // library: 'WxCanvas2d'
     },
     watch: true,
     watchOptions: {
         aggregateTimeout: 300,
-        ignored: './plugin',
+        ignored: [
+            './.vscode',
+            './doc',
+            './docs',
+            './miniprogram',
+            './node_modules',
+            './plugin/index.min.js',
+            './plugin/index.min.js.map',
+            './plugin/build/'
+        ],
         poll: 1000
     },
     devtool: 'source-map',
